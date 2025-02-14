9edd1261a528b5b0b96dcc1974c2ef8c770040a2
Memaansa <139412028+Memaansa@users.noreply.github.com>
2025-02-14 17:21:13 +0000

    Pruebas resueltas

--- Tester.py
+++ Tester.py
@@ -8,28 +8,28 @@ def manual():
 
 @pytest.fixture
 def sensor ():
-    return Sensores()
+    return Sensores(test_mode=True)
 
 def test_estado_inicial_interruptor(manual):    
     assert manual.state is False  # Estado inicial, debe estar apagado
 
-def test_encendido_apagado(manual):       
-    manual.encender()
+def test_estado_inicial(manual):       
+    manual.señal("ON") 
     assert manual.state is True  # Debe encenderse
     
 def test_apagado(manual):  
-    manual.apagar()
+    manual.señal("OFF")  
     assert manual.state is False  # Debe apagarse
 
 def test_sensor_initial_state(sensor):  
     assert sensor.luz_on is False  # Al inicio, debe estar apagado
 
 def test_sensor_detect_person(sensor):    
-    sensor.detectar_persona()
+    sensor.hay_persona(True)
     assert sensor.luz_on is True  # Debe encenderse si hay una persona
 
 def test_sensor_no_person(sensor):    
-    sensor.no_hay_persona()
+    sensor.hay_persona(False)
     assert sensor.luz_on is False  # Debe apagarse si no hay nadie
 
 
